3b0bdf49ad83c6dd9e3e3cf13649f97428fe7f60
Ison <ison.chang@gmail.com>
2016-02-22 11:36:31 +0800

    werwer

--- wp-content/themes/roger/footer.php
+++ wp-content/themes/roger/footer.php
@@ -1,8 +1,8 @@
 </div>
 <div id="footer">
 <div class="copyright">
-	<div class="logo"></div>
-	<div class="word">Copyright©hantanghongmu.com. 2015 All rights reserved.</div>
+	<a href="javascript:location.reload()"><div class="logo"></div></a>
+	<div class="word"><a style="color:#ccc;text-decoration:none" href="javascript:location.reload()">©</a> Copyright©hantanghongmu.com. 2015 All rights reserved.</div>
 	<div class="clear"></div>
 </div>
 </div>
